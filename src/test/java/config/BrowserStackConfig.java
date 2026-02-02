0d4ff93b80cc5129d7f41f0bd6344163ed08a910
matve <230432@astanait.edu.kz>
2026-02-02 20:25:57 +0500

    Its fine now

--- src/test/java/config/BrowserStackConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-public class BrowserStackConfig {
-    public static final String USERNAME = "bsuser_xiqolK";
-    public static final String ACCESS_KEY = "2ss5GZ6v7p71t4HznWgh";
-
-    public static final String HUB_URL =
-            "https://" + USERNAME + ":" + ACCESS_KEY + "@hub.browserstack.com/wd/hub";
-}
